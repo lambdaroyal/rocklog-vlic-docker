3e5d6141eadda460868cc1136cc3631cfda3cb8e
gixxi <christian.meichsner@live.com>
2019-11-28 15:52:17 +0100

    FEATURE: New buildserver based on npm v13

--- vlic_maker/start.sh
+++ vlic_maker/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro vlic/vlic_maker:v4
+mkdir -p repository
+sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro hub5.planet-rocklog.com:5000/vlic/vlic_maker:v5
