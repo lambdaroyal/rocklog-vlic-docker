581d23f59f11bffeabf93667622f85f5e350bb7f
Christian Meichsner <christian.meichsner@live.com>
2019-12-30 23:13:21 +0100

    FEATURE: inherit start.sh calling user rights for proper ssh keypair consideration

--- vlic_maker/start.sh
+++ vlic_maker/start.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+!/bin/bash
 mkdir -p repository
-sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro hub5.planet-rocklog.com:5000/vlic/vlic_maker:v5
+_user=$(stat -c '%U' .)
+_version="v5"
+sudo docker pull hub5.planet-rocklog.com:5000/vlic/vlic_maker:$_version
+sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro -v repository:/var/buildbot/repository:rw hub5.planet-rocklog.com:5000/vlic
+/vlic_maker:$_version $_user $(id -u $_user)
