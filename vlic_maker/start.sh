8c0e4f590c5f54223d551186dd5e9f95a7223384
gixxi <christian.meichsner@live.com>
2020-09-30 23:09:14 +0200

    updated maker

--- vlic_maker/start.sh
+++ vlic_maker/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro vlic/vlic_maker:v4
+mkdir -p repository
+_user=$(stat -c '%U' .)
+_version="v5"
+sudo docker pull hub5.planet-rocklog.com:5000/vlic/vlic_maker:$_version
+sudo docker run --name=vlic_maker -itd -p 23:22 -p 81:80 -v ~/.ssh:/var/buildbot/.ssh:ro -v repository:/var/buildbot/repository:rw hub5.planet-rocklog.com:5000/vlic/vlic_maker:$_version $_user $(id -u $_user)
